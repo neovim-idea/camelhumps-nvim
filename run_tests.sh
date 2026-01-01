de9ae6ddd64cf913c118a8c2a1c5f4bf3cf70760
Diego Casella <polentino911@gmail.com>
2026-01-02 00:31:58 +0100

    fix: cleanup run_tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,15 +4,6 @@ set -euo pipefail
 ROOT="$(cd "$(dirname "$0")" && pwd)"
 cd "$ROOT"
 
-# exec "busted" "$ROOT/tests/spec"
-
-
-# nvim --headless -c "PlenaryBustedDirectory tests/ { minimal_init = 'tests/minimal_init.lua' }"
-
-
-# nvim --headless -u tests/minimal_init.lua -l tests/run_busted.lua
-# exec nvim --headless -u tests/minimal_init.lua -l "$@"``
-# busted --lua=./scripts/nlua tests
 LUADIR="$(brew --prefix luajit)"
 luarocks --lua-dir="$LUADIR" --lua-version=5.1 --tree=.rocks install busted
 eval "$(luarocks --lua-dir="$LUADIR" --lua-version=5.1 --tree="$(pwd)/.rocks" path --bin)"
