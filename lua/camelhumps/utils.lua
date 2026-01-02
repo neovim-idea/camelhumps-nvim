88ce1fb7a2bef71be6939e80f67f5bccfabe2815
Diego Casella <polentino911@gmail.com>
2026-01-02 23:28:07 +0100

    fix: start implementation of right jump

--- lua/camelhumps/utils.lua
+++ lua/camelhumps/utils.lua
@@ -10,6 +10,19 @@ function Utils.rightmost_word(line)
 end
 
 --[[
+-- Given a non-empty string parameter called `line`, it tries to fetch the leftmost, non-whitespace word available and
+-- return it, along with its starting index.
+--]]
+function Utils.leftmost_word(line)
+    local start_index, word = line:match("^%s*()(%S+)")
+    if not word then
+        return nil, nil
+    end
+    -- local ending_index = start_index + #word - 1
+    return word, start_index
+end
+
+--[[
 -- Given a non-empty string parameter called `word`, it will try to tokenize it according two categories: alphanumerical
 -- and special character words, and return the last token found. This token will become the possible candidate where we
 -- will search the exact place to camel-hump from right to left direction.
@@ -42,4 +55,34 @@ function Utils.last_token(word)
   return token
 end
 
+--[[
+-- Given a non-empty string parameter called `word`, it will try to tokenize it according to two categories:
+-- alphanumerical and special character words, and return the first token found. This token will become the possible
+-- candidate where we will search the exact place to camel-hump from left to right direction.
+--]]
+function Utils.first_token(word)
+  local i = 1
+  local len = #word
+
+  if len == 0 then
+    return nil
+  end
+
+  local c = word:sub(i, i)
+  local j = i
+
+  if c:match("%w") then
+    -- alphanumerical run
+    while j <= len and word:sub(j, j):match("%w") do
+      j = j + 1
+    end
+  else
+    -- non-alphanumerical run
+    while j <= len and not word:sub(j, j):match("%w") do
+      j = j + 1
+    end
+  end
+
+  return word:sub(i, j - 1)
+end
 return Utils
