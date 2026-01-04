bcf8fbe13f0ba4b16646791f121e098fb9e99c40
Diego Casella <polentino911@gmail.com>
2026-01-04 17:09:26 +0100

    feat: added left/right delete actions as well

--- lua/camelhumps/init.lua
+++ lua/camelhumps/init.lua
@@ -2,16 +2,24 @@ local CamelHumps = {}
 
 local logic = require("camelhumps.logic")
 
-function CamelHumps.left()
-  logic.left_camel_hump()
-end
-
 function CamelHumps.setup()
   return CamelHumps
 end
 
+function CamelHumps.left()
+  logic.left_camel_hump()
+end
+
 function CamelHumps.right()
   logic.right_camel_hump()
+end
+
+function CamelHumps.left_delete()
+  logic.left_delete()
+end
+
+function CamelHumps.right_delete()
+  logic.right_delete()
 end
 
 return CamelHumps
